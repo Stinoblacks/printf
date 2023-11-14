0eafa420f33c383c9f7b0391db20ce90d5598586
Saidi Sodiq <saidisodiq96@gamil.com>
2023-11-14 14:42:15 +0100

    fixed some betty issues

--- printInteger.c
+++ printInteger.c
@@ -4,42 +4,42 @@
 * @args: argument to print
 * Return: the integers to be printed
 */
-int printInt(va_list args) 
+int printInt(va_list args)
 {
-    int n = va_arg(args, int);
-    int remainingDigits, digit, exp = 1;
-    int chars_printed = 0;
-    int isNegative = 0;
+	int n = va_arg(args, int);
+	int remainingDigits, digit, exp = 1;
+	int chars_printed = 0;
+	int isNegative = 0;
 
-    if (n < 0)
-    {
-        _putchar('-');
-        n = -n;
-        chars_printed++;
-        isNegative = 1;
-    }
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+		chars_printed++;
+		isNegative = 1;
+	}
 
-    if (n == 0)
-    {
-        _putchar('0');
-        return (1);
-    }
+	if (n == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
 
-    remainingDigits = n;
+	remainingDigits = n;
 
-    while (remainingDigits / exp > 9)
-    {
-        exp *= 10;
-    }
+	while (remainingDigits / exp > 9)
+	{
+		exp *= 10;
+	}
 
-    while (exp > 0)
-    {
-        digit = remainingDigits / exp;
-        _putchar(digit + '0');
-        chars_printed++;
-        remainingDigits -= digit * exp;
-        exp /= 10;
-    }
+	while (exp > 0)
+	{
+		digit = remainingDigits / exp;
+		_putchar(digit + '0');
+		chars_printed++;
+		remainingDigits -= digit * exp;
+		exp /= 10;
+	}
 
-    return (chars_printed + isNegative);
+	return (chars_printed + isNegative);
 }
