8d8b2b5904c9867af0f5869502985b5171d074b2
Stinoblacks <austineokpegwa@gmail.com>
2023-11-15 05:58:35 -0800

    A custom printf function that prints charawcter, strings, and percent sign

--- print_decimal.c
+++ print_decimal.c
@@ -1,47 +1,129 @@
 #include "main.h"
 /**
-* print_decimal - prints decimal
-* @args: argument to print
-* Return: integer (the number of characters printed)
-*/
-int print_decimal(va_list args)
+ * print_hex - prints an unsigned int in hexidecimal form
+ * @n: unsigned int to print
+ * @c: flag to determine case of printing (0 = lower, 1 = upper)
+ *
+ * Return: number of digits printed
+ */
+int print_hex(unsigned int n, unsigned int c)
 {
-	int n = va_arg(args, int);
-	int num, last = n % 10, digit;
-	int i = 1;
-	int exp = 1;
+	unsigned int a[8];
+	unsigned int i, m, sum;
+	char diff;
+	int count;
 
-	n = n / 10;
-	num = n;
-
-	if (last < 0)
+	m = 268435456; /* (16 ^ 7) */
+	if (c)
+		diff = 'A' - ':';
+	else
+		diff = 'a' - ':';
+	a[0] = n / m;
+	for (i = 1; i < 8; i++)
 	{
-		_putchar('-');
-		num = -num;
-		n = -n;
-		last = -last;
-		i++;
+		m /= 16;
+		a[i] = (n / m) % 16;
 	}
-
-	if (num > 0)
+	for (i = 0, sum = 0, count = 0; i < 8; i++)
 	{
-		while (num / 10 != 0)
+		sum += a[i];
+		if (sum || i == 7)
 		{
-			exp = exp * 10;
-			num = num / 10;
-		}
-		num = n;
-
-		while (exp > 0)
-		{
-			digit = num / exp;
-			_putchar(digit + '0');
-			num = num - (digit * exp);
-			exp = exp / 10;
-			i++;
+			if (a[i] < 10)
+				_putchar('0' + a[i]);
+			else
+				_putchar('0' + diff + a[i]);
+			count++;
 		}
 	}
-	_putchar(last + '0');
+	return (count);
+}
+/**
+ * print_x - takes an unsigned int and prints it in lowercase hex notation
+ * @x: unsigned int to print
+ *
+ * Return: number of digits printed
+ */
+int print_x(va_list x)
+{
+	return (print_hex(va_arg(x, unsigned int), 0));
+}
 
-	return (i);
+/**
+ * print_X - takes am unsigned int and prints it in uppercase hex notation
+ * @X: unsigned int to print
+ *
+ * Return: number of digits printed
+ */
+int print_X(va_list X)
+{
+	return (print_hex(va_arg(X, unsigned int), 1));
+}
+
+/**
+ * _pow - calculates an exponent
+ * @base: base of exponent
+ * @exponent: exponent of number
+ *
+ * Return: base ^ exponent
+ */
+static unsigned long _pow(unsigned int base, unsigned int exponent)
+{
+	unsigned int i;
+	unsigned long ans = base;
+
+	for (i = 1; i < exponent; i++)
+	{
+		ans *= base;
+	}
+	return (ans);
+}
+
+/**
+ * print_p - prints an address
+ * @p: address to print
+ *
+ * Return: number of characters to print
+ */
+int print_p(va_list p)
+{
+	int count = 0;
+	unsigned int a[16];
+	unsigned int i, sum;
+	unsigned long n, m;
+	char *str = "(nil)";
+
+	n = va_arg(p, unsigned long);
+	if (n == 0)
+	{
+		for (i = 0; str[i]; i++)
+		{
+			_putchar(str[i]);
+			count++;
+		}
+		return (count);
+	}
+	_putchar('0');
+	_putchar('x');
+	count = 2;
+	m = _pow(16, 15); /* 16 ^ 15 */
+	a[0] = n / m;
+	for (i = 1; i < 16; i++)
+	{
+		m /= 16;
+		a[i] = (n / m) % 16;
+	}
+	for (i = 0, sum = 0; i < 16; i++)
+	{
+		sum += a[i];
+		if (sum || i == 15)
+		{
+			if (a[i] < 10)
+				_putchar('0' + a[i]);
+			else
+				_putchar('0' + ('a' - ':') + a[i]);
+			count++;
+		}
+	}
+	return (count);
 }
--- print_mod.c
+++ print_mod.c
@@ -1,10 +1,15 @@
 #include "main.h"
+#include <stdarg.h>
+
 /**
-* print_percent - prints the char %.
-* Return: 1 (the number of characters printed).
-*/
-int print_mod(void)
+ * print_mod - Print a percentage sign.
+ * @args: Argument list (unused).
+ *
+ * Return: Always returns 1.
+ */
+int print_mod(va_list args)
 {
+	(void)args;
 	_putchar('%');
-	return (1);
+	return 1;
 }
